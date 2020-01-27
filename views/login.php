2b4362454123e720ed571ed92c5ddbd4f54f9c3c
Allan Camargo <allan.cdev@gmail.com>
2020-01-27 15:15:00 -0300

    add login page

--- views/login.php
+++ views/login.php
@@ -1,105 +1,110 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="pt-BR">
 <head>
+	<title>Control_A - Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/vendor/font-awesome-4.7.0/css/font-awesome.min.css">
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="Allan Cristian">
+	<link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/vendor/Linearicons-Free-v1.0.0/icon-font.min.css">
 
-  <title>Control_A - Login</title>
+	<link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/vendor/animate/animate.css">
+	
+	<link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/vendor/animsition/css/animsition.min.css">
 
-  <!-- Custom fonts for this template-->
-  <link href="<?= BASE_URL ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="<?= BASE_URL ?>/assets/css/template.min.css" rel="stylesheet">
-  <link href="<?= BASE_URL ?>/assets/css/login.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/assets/css/util.css">
+	<link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/assets/css/main.css">
 
 </head>
+<body style="background-color: #666666;">
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form user"  id = "user"  method = "POST">
+					<span class="login100-form-title p-b-43">
+						Faça login para continuar
+					</span>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate = "Digite o código da empresa: ">
+						<input class="input100" type="text" id="companies" name="companies" >
+						<span class="focus-input100"></span>
+						<span class="label-input100">Empresa</span>
+					</div>
+					
+					<div class="wrap-input100 validate-input" data-validate = "E-mail válido é necessário: ex@abc.xyz">
+						<input class="input100" type="text" id="email" name="email" >
+						<span class="focus-input100"></span>
+						<span class="label-input100">E-mail</span>
+					</div>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate="Senha obrigatória">
+						<input class="input100" type="password" id="i_password" name="i_password" >
+						<span class="focus-input100"></span>
+						<span class="label-input100">Senha</span>
+					</div>
 
-<body class="bg-gradient-primary">
+					<div class="flex-sb-m w-full p-t-3 p-b-32">
+						<div class="contact100-form-checkbox">
+							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+							<label class="label-checkbox100" for="ckb1">
+								Lembrar
+							</label>
+						</div>
 
-  <div class="container">
+						<div>
+							<a href="#" class="txt1">
+								Esqueceu sua senha?
+							</a>
+						</div>
+					</div>
+			
 
-    <!-- Outer Row -->
-    <div class="row justify-content-center">
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Entrar
+						</button>
+					</div>
+					
+					<div class="text-center p-t-46 p-b-20">
+						<span class="txt2">
+							ou inscreva-se
+						</span>
+					</div>
 
-      <div class="col-xl-10 col-lg-12 col-md-9">
+					<div class="login100-form-social flex-c-m">
+						<a href="#" class="login100-form-social-item flex-c-m bg1 m-r-5">
+							<i class="fa fa-linkedin" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
 
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
-            <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Bem vindo!</h1>
-                  </div>
-                  <form class="user" id = "user" method="POST">
-                    <div class="form-group">
-                      <input type="text" class="form-control form-control-user" id="companies" name="companies" aria-describedby="companiesHelp" placeholder="Digite o id da empresa...">
-                    </div>
-                    <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="email" name="email" aria-describedby="emailHelp" placeholder="Digite seu email...">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="i_password" name="i_password" placeholder="Digite sua senha...">
-                    </div>
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Lembrar Me</label>
-                      </div>
-                    </div>
-                      <?php
-                      if (isset($error) && !empty($error)) {
-                        ?>
-                          <div class="card mb-4 py-3 border-left-danger">
-                            <div class="card-body">
-                                <?= $error ?>
-                            </div>
-                          </div>
-                        <?php
-                      }
-                      ?>
-                    <a href="#" onclick = "document.getElementById('user').submit()" class="btn btn-primary btn-user btn-block">
-                      Entrar
-                    </a>
-                  </form>
-                  <hr>
+				<div class="login100-more" style="background-image: url('<?= BASE_URL ?>/assets/images/bg-01.jpg');">
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
 
-                  <div class="text-center">
-                    <a class="small" href="forgot-password.html">Esqueceu sua senha?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="register.html">Criar conta!</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-    </div>
-
-  </div>
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="<?= BASE_URL ?>/vendor/jquery/jquery.min.js"></script>
-  <script src="<?= BASE_URL ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- Core plugin JavaScript-->
-  <script src="<?= BASE_URL ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="<?= BASE_URL ?>/assets/js/sb-admin-2.min.js"></script>
+	
+	
+	<script src="<?= BASE_URL ?>/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="<?= BASE_URL ?>/vendor/animsition/js/animsition.min.js"></script>
+	<script src="<?= BASE_URL ?>/vendor/bootstrap/js/popper.js"></script>
+	<script src="<?= BASE_URL ?>/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="<?= BASE_URL ?>/vendor/select2/select2.min.js"></script>
+	<script src="<?= BASE_URL ?>/vendor/daterangepicker/moment.min.js"></script>
+	<script src="<?= BASE_URL ?>/vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="<?= BASE_URL ?>/vendor/countdowntime/countdowntime.js"></script>
+	<script src="<?= BASE_URL ?>/assets/js/main.js"></script>
 
 </body>
-
 </html>
